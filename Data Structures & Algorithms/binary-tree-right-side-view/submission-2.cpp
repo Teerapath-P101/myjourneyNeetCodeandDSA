df87840bc970b01e03fdc9fd131a134d543e4020
Tae__ <85441805+Teerapath-P101@users.noreply.github.com>
2026-05-11 23:55:45 +0700

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// BFS
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
+        vector<int> res;
+        queue<TreeNode*> q;
+        q.emplace(root);
+        while (!q.empty()) {
+            int qsize = q.size();
+            for (int i=0; i<qsize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->right) q.emplace(node->right);
+                if (node->left) q.emplace(node->left);
+                if (i == 0) res.push_back(node->val);
+            }
+        }
+        return res;
+    }
+};
