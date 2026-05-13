6560b0560a05200f0d592965d8de6f7c8a40fda1
Tae__ <85441805+Teerapath-P101@users.noreply.github.com>
2026-05-13 23:57:19 +0700

    Add: kth-smallest-integer-in-bst - submission-6

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-6.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// Morris Traversal
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        TreeNode* curr = root;
+        while(curr) {
+            if (curr->left) {
+                TreeNode* wayBack = curr->left;
+                while (wayBack->right && wayBack->right != curr) wayBack = wayBack->right;
+                if (!wayBack->right) {
+                    wayBack->right = curr;
+                    curr = curr->left;
+                } else {
+                    wayBack->right = nullptr;
+                    k--;
+                    if (k == 0) return curr->val;
+                    curr = curr->right;
+                }
+            } else { // Reach the kth smallest
+                k--;
+                if (k == 0) return curr->val;
+                curr = curr->right;
+            }
+        }
+        return -1;
+    }
+};
