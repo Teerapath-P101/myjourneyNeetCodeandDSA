5ad0d5b76d3e2fb20d3bd7517d363f267cbe5e3a
Tae__ <85441805+Teerapath-P101@users.noreply.github.com>
2026-05-18 23:26:18 +0700

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.cpp
@@ -0,0 +1,29 @@
+// Dynamic programming
+// (it's consider to be Bottom-Up too) Time complexity: O(n), Space complexity: O(1)
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n <= 2) return n;
+//         int one = 1, two = 2; // two is current step
+//         for (int i=3; i <= n; i++) {
+//             int tmp = one + two;
+//             one = two;
+//             two = tmp;
+//         }
+//         return two;
+//     }
+// };
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int one = 1, two = 1; // one is current step two is the step before that
+                              // `one` is ALWAYS one step ahead of `two`!
+        for (int i=0; i < n-1; i++) {
+            int tmp = one;
+            one += two; // the current step to be added with the two
+            two = tmp; // (the previos step which old the one)
+        }
+        return one;
+    } //The Window Sliding Visualization
+};
