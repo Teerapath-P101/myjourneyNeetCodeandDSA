ab08556f34ce4902f6a1b7584da41ac515c48584
Tae__ <85441805+Teerapath-P101@users.noreply.github.com>
2026-05-19 19:34:03 +0700

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.cpp
@@ -0,0 +1,16 @@
+// Reduce repetitive tasks by Dynamic Programming
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        int offset = 1;
+        vector<int> res(n + 1);
+        for (int i=1; i<=n; i++) {
+            if (offset * 2 == i) {
+                offset = i;
+            }
+            res[i] = 1 + res[i - offset];
+        }
+        return res;
+    }
+};
