405dc2801be3944e07fa6706e572b0c3dca2e17a
Tae__ <85441805+Teerapath-P101@users.noreply.github.com>
2026-05-29 11:05:17 +0700

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cpp
@@ -0,0 +1,10 @@
+// Converting to Sorted Set
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> unique(nums.begin(), nums.end());
+        int i = 0;
+        for(int num : unique) nums[i++] = num; 
+        return unique.size();
+    }
+};
