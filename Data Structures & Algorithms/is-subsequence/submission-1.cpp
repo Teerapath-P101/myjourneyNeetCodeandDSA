5aaa660f0953e1fc4900628a870ac7937f3aa38c
Tae__ <85441805+Teerapath-P101@users.noreply.github.com>
2026-07-12 00:58:50 +0700

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.cpp
@@ -0,0 +1,16 @@
+// Two pointers
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+       int i = 0, j = 0;
+       while (i < s.length() && j < t.length()) {
+            if (s[i] == t[j]) {
+                i++;
+
+            }
+            j++;
+       }
+       if (i < s.length()) return false;
+       else return true;
+    }
+};
